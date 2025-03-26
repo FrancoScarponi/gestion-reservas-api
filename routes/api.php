8694ab54a4be7457b570bbfee88e38477070ae9a
FrancoScarponi <133600838+FrancoScarponi@users.noreply.github.com>
2025-03-26 17:31:38 -0300

    agregando comentarios

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,11 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+//Public routes
 Route::post('/register', [UserController::class, 'register'])->name('register');
 Route::post('/login', [UserController::class, 'login'])->name('login');
 
+//Rutas con auth
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'checkAuth'])->name('');
 
@@ -20,7 +21,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('reservation', ReservationController::class)->except(['show', 'update']);
     Route::get('reservation/schedule', [ReservationController::class, 'getSchedule'])->name('reservation.schedule');
 
-    
+    //Rutas de admin
     Route::middleware('role:admin')->group(function () {
         Route::get('reservation/pending', [ReservationController::class, 'indexPending'])->name('reservation.pending');
         Route::put('reservation/{id}', [ReservationController::class, 'changeStatus'])->name('reservation.changeStatus');
